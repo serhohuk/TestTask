a553927dd2a5cadf956883f0738b5c6f6ade9934
Serhii Hrihorchuk <gosergio27@gmail.com>
2021-08-11 21:53:57 +0300

    work but without viewPager and notifications

--- app/src/main/java/com/sign/pecodesoftwaretest/MainFragment.kt
+++ app/src/main/java/com/sign/pecodesoftwaretest/MainFragment.kt
@@ -1,20 +1,49 @@
 package com.sign.pecodesoftwaretest
 
+import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_main_layout.*
 
 class MainFragment : Fragment(R.layout.fragment_main_layout) {
 
+   val args : MainFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.e("STARTED", "NEW FRAGMENT STARTED")
+        Log.e("STARTED","STARTED FRAGMENT ${args.fragmentNumber}")
+        setFragmentNumber()
+        hideRemoveButton()
 
-//        button.setOnClickListener {
-//            findNavController().navigate(R.id.mainFragment)
-//        }
+        btn_add.setOnClickListener {
+            val numberForArgs = tv_num.text.toString().toInt()+1
+            val action = MainFragmentDirections.actionMainFragmentSelf(numberForArgs)
+            findNavController().navigate(action)
+        }
+
+        btn_remove.setOnClickListener {
+            val numberForArgs = tv_num.text.toString().toInt()-1
+            val action = MainFragmentDirections.actionMainFragmentSelf(numberForArgs)
+            findNavController().popBackStack(R.id.mainFragment,true)
+            //findNavController().navigate(action)
+
+        }
+
+    }
+
+    private fun setFragmentNumber(){
+        if (args.fragmentNumber!=0){
+            tv_num.text = args.fragmentNumber.toString()
+        }
+    }
+
+    private fun hideRemoveButton(){
+        if (tv_num.text.toString()=="1"){
+            btn_remove.visibility = View.INVISIBLE
+        }
     }
 }
